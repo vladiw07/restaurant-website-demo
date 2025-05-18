8009c4c62c33a8b4a4f7b77d07369d64387f8bb7
vladiw07 <onevlade44@gmail.com>
2025-05-18 12:20:16 +0300

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
